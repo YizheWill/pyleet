c5f25a7a4cfcc52d8ba55305f10927b7c3f99b85
yizhewill <will.yizhewang@gmail.com>
2021-09-12 17:58:39 -0700

    is valid sbusequence

---
+++ sortedSquaredArray.py
@@ -0,0 +1,15 @@
+def sortedSquaredArray(array):
+    # i = 0
+    # j = len(array) - 1
+    # res = []
+    # while i <= j:
+    #     if abs(array[j]) > abs(array[i]):
+    #         res = array[j] * array[j] + res
+    #         j -= 1
+    #     else:
+    #         res = array[i] * array[i] + res
+    #         i += 1
+    # return res
+    res = [x * x for x in array]
+    res.sort()
+    return res
