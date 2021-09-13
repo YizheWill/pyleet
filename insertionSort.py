3392828edfb35abed99ae495e8c97dcfde9ff544
yizhewill <will.yizhewang@gmail.com>
2021-09-13 14:37:51 -0700

    update insertion sort

--- insertionSort.py
+++ insertionSort.py
@@ -1,7 +1,6 @@
 def insertionSort(array):
-    # Write your code here.
-    for i in range(len(array) - 1):
-        for j in range(i + 1, len(array)):
-            if array[j] < array[i]:
-                array[i], array[j] = array[j], array[i]
-    return array
+	for i in range(len(array)):
+		for j in range(i, 0, -1):
+			if array[j] < array[j - 1]:
+				array[j], array[j - 1] = array[j - 1], array[j]
+	return array
