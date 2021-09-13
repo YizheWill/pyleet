316d8c1bb9dc1da037427a12802320750b50434d
yizhewill <will.yizhewang@gmail.com>
2021-09-13 08:56:19 -0700

    add while loop solution to bfs

--- productSum.py
+++ productSum.py
@@ -7,3 +7,18 @@ def productSum(array, depth=1):
         else:
             res += ele
     return res * depth
+
+# whileloop
+# def binarySearch(array, target):
+#     # Write your code here.
+# 	left = 0
+# 	right = len(array) - 1
+# 	while left <= right:
+# 		mid = (left + right) // 2
+# 		if array[mid] == target:
+# 			return mid
+# 		elif target < array[mid]:
+# 			right = mid - 1
+# 		else:
+# 			left = mid + 1
+# 	return -1
